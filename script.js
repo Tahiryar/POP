7a2e12d31f82097f07e9bb852b1a5a838bfb428e
Tahir Yar <tahiryar.251k@gmail.com>
2024-10-18 16:20:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup"); // Corrected selector
+
+function OpenPopup() {
+    popup.classList.add("open-popup");
+}
+
+function ClosePopup() {
+    popup.classList.remove("open-popup");
+}
